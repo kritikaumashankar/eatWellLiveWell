3fc1aab70448fdd27226f00d322ddf5de9eddc0e
Kritika Umashankar <kritikaumashankar@gmail.com>
2021-01-04 11:17:19 -0700

    added env variable for url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = {
   env:{
-    PUBLIC_URL: "http://localhost:3000/",
+    PUBLIC_URL: process.env.PUBLIC_URL,
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
   }
 }
