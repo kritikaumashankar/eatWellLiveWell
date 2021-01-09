77307094e4b5ecaff9a40e7965945e10d7dfa0d8
Kritika Umashankar <kritikaumashankar@gmail.com>
2021-01-08 20:11:18 -0700

    email fix

--- pages/api/send.js
+++ pages/api/send.js
@@ -3,12 +3,12 @@ const sgMail = require('@sendgrid/mail')
 
 export default async function(req, res) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-  console.log(process.env.SENDGRID_API_KEY)
+  //console.log(process.env.SENDGRID_API_KEY)
   const { fname, lname,email,phone, message } = req.body
   console.log(process.env.USER_EMAIL)
   const content = {
-    to: process.env.USER_EMAIL,
-    from: process.env.USER_EMAIL,
+    to: 'sushrams10@gmail.com',
+    from: 'sushrams10@gmail.com',
     subject: `New Message From - ${email}`,
     text: message,
     html: `<p>Hi! My name is ${fname} ${lname}</p>
