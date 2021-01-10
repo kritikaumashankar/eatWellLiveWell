099034c9508d635e7c3ede28e19f70e1f1e178ce
Kritika Umashankar <kritikaumashankar@gmail.com>
2021-01-09 17:23:31 -0700

    Modified favicon

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => (
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Eat Well, Live Well | Sushma Ramaswamy </title>
         <link rel = "stylesheet" href= "https://bootswatch.com/4/minty/bootstrap.min.css" />
-        <link rel="icon" href="/logo.jpg" />
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital@1&display=swap" rel="stylesheet" />
